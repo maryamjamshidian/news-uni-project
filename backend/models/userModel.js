87699eaedfc807deb73b1e8dace4c3545dfd4590
maryam <marryam.jam78@gmail.com>
2023-09-07 16:59:25 +0330

    ایجاد usermodel و تعریف اسم جدول و فیلد های ان

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database";
+
+const {DataTypes}=Sequelize;
+
+const Users = db.define("users",{
+name:{
+    type:DataTypes.STRING,
+},
+email:{
+    type:DataTypes.STRING,
+},
+password:{
+    type:DataTypes.STRING,
+},
+refresh_token:{
+    type:DataTypes.TEXT,
+},
+image:{
+    type:DataTypes.STRING,
+},
+url:{
+    type:DataTypes.STRING,
+},
+isAdmin:{
+    type:DataTypes.BOOLEAN,
+    defaultValue:false,
+},
+},{freezeTableName:true,})
+
+
+export default Users;
