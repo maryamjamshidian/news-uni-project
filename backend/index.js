34051973d6cfe8a0f8668bc606bd12e39785aa51
maryam <marryam.jam78@gmail.com>
2023-09-07 17:25:44 +0330

    اضافه کردن استفاده ار userRoutes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import db from './config/Database.js';
+import userRoutes from './routes/userRoutes.js';
 const app = express();
 
 try {
     await db.authenticate();
     console.log("database conneted");
 } catch (error) {
-    console.log(error);
-    
+    console.log(error);   
 }
-
+app.use(userRoutes)
 app.listen(5000,()=>console.log("server runnig"))
