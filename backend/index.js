0713f9d6073ff3a6ff4182d8892d639ae9ca4aae
maryam <marryam.jam78@gmail.com>
2023-09-07 17:43:29 +0330

    وصل کردن روت و کنترلر و مدل به اندکس جی اس

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import db from './config/Database.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import db from "./config/Database.js";
+import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
 try {
-    await db.authenticate();
-    console.log("database conneted");
+  await db.authenticate();
+  console.log("database conneted");
+//   await db.sync();
 } catch (error) {
-    console.log(error);   
+  console.log(error);
 }
-app.use(userRoutes)
-app.listen(5000,()=>console.log("server runnig"))
+app.use(userRoutes);
+app.listen(5000, () => console.log("server runnig"));
