d1b94747b0cc8740f9977abecb900ca3f7577bc1
maryam <marryam.jam78@gmail.com>
2023-09-08 22:19:05 +0330

    add login routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {Register, getAllUsers } from "../controllers/userController.js";
+import {Login, Register, getAllUsers } from "../controllers/userController.js";
 
 const router=express.Router();
 
 router.get("/api/users",getAllUsers)
 router.post("/api/users/register",Register)
+router.post("/api/users/login",Login)
 
 
 export default router;
