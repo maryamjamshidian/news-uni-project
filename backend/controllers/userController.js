8353fe7e173d5f5070683ed0c15a5782d0488970
maryam <marryam.jam78@gmail.com>
2023-09-08 15:20:57 +0330

    check password=onfPassword &  add hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import Users from "../models/userModel.js";
-
+import bcrypt from "bcrypt"
 export const getAllUsers = async (req, res) => {
   try {
     const users = Users.findAll({});
@@ -10,11 +10,23 @@ export const getAllUsers = async (req, res) => {
 };
 
 export const Register = async (req, res) => {
-  try {
-    const { name, email, password, confPassword, isAdmin } = req.body;
+  const { name, email, password, confPassword, isAdmin } = req.body;
+  if(password!==confPassword){
+    return res.json("پسورد و تکرار آن باهم برابر نیست")
+  }
+  const salt= await bcrypt.genSalt();
+  const hashPassword= await bcrypt.hash(password,salt);
 
-    console.log(name, email, password, confPassword, isAdmin);
-    res.json("register");
+  try {
+await Users.create({
+  name:name,
+  email:email,
+  password:hashPassword,
+  isAdmin:isAdmin
+})
+res.json("ثبت نام موفقیت آمیر بود")
+    // console.log(name, email, password, confPassword, isAdmin);
+    // res.json("register");
   } catch (error) {
     console.log(error);
   }
