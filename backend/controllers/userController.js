cbb769a3b321d27165c54c316c0062c0db2b6e4f
maryam <marryam.jam78@gmail.com>
2023-09-08 14:45:23 +0330

    add register funtion

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,21 @@
 import Users from "../models/userModel.js";
 
-export const getAllUsers=async(req,res)=>{
-
-try {
-    const users=Users.findAll({});
-    res.json(users)
-
-} catch (error) {
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = Users.findAll({});
+    res.json(users);
+  } catch (error) {
     console.log(error);
-}
+  }
+};
 
-}
+export const Register = async (req, res) => {
+  try {
+    const { name, email, password, confPassword, isAdmin } = req.body;
+
+    console.log(name, email, password, confPassword, isAdmin);
+    res.json("register");
+  } catch (error) {
+    console.log(error);
+  }
+};
