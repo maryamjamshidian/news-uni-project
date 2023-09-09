306264be2d86c90b2c357d7763db5de3324078e1
maryam <marryam.jam78@gmail.com>
2023-09-09 06:24:44 +0330

    add dotnev &cookie parser paCkage

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import db from "./config/Database.js";
 import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
 const app = express();
 
 try {
   await db.authenticate();
   console.log("database conneted");
-//   await db.sync();
+  //   await db.sync();
 } catch (error) {
   console.log(error);
 }
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 app.use(userRoutes);
+
+
+
 app.listen(5000, () => console.log("server runnig"));
