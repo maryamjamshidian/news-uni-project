09ac889d6968f7a118fbb4c226929d435efc3cf5
maryam <marryam.jam78@gmail.com>
2023-09-09 12:21:33 +0330

    ایجاد فایل verifyToken

---
+++ backend/middleware/VerifyTokne.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+
+//const res = axios.post("/api/test", {
+// headers: {
+  //   "authorization": "Bearer asdasdasdasdasd"
+//}
+//})
+export const verifyToken = (req, res, next) => {
+     const authHeader = req.headers["authorization"];
+     const token = authHeader && authHeader.split(" ")[1]
+     if(token == null) return res.status(401).json("شما باید ابتدا وارد حساب کاربری خود شوید")
+     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=> {
+          if(err) return res.json("توکن منقضی شده است")
+          next();
+     })
+}
