17e7223413c619a4354c66da5048b4b39665f55e
maryam <marryam.jam78@gmail.com>
2023-09-17 22:10:13 +0330

    add vrerfyToken in routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {Login, Register, getAllUsers } from "../controllers/userController.js";
+import { verifyToken } from './../middleware/VerifyTokne.js';
 
 const router=express.Router();
+// router.get("/token", refreshToken)
 
-router.get("/api/users",getAllUsers)
+router.get("/api/users", verifyToken,getAllUsers)
 router.post("/api/users/register",Register)
 router.post("/api/users/login",Login)
 
