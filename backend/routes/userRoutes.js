854ea909a4b322ff206331f8fa6324f8d76ef1ec
maryam <marryam.jam78@gmail.com>
2023-09-17 22:33:34 +0330

    add refresh token ruotes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {Login, Register, getAllUsers } from "../controllers/userController.js";
 import { verifyToken } from './../middleware/VerifyTokne.js';
+import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router=express.Router();
-// router.get("/token", refreshToken)
+router.get("/token", refreshToken)
 
 router.get("/api/users", verifyToken,getAllUsers)
 router.post("/api/users/register",Register)
