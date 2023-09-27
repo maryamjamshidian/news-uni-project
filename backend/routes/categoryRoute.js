dce2eac1208cc7e225afd76bce8bf3378013efbc
maryam <marryam.jam78@gmail.com>
2023-09-27 07:44:45 +0330

    add update & delete Category in route

--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,10 +1,12 @@
 import express from "express"
 import {verifyToken} from "../middleware/VerifyTokne.js"
-import { createCategory, getCategory } from "../controllers/CategoryControlle.js"
+import { createCategory, deleteCategory, getCategory, updateCategory } from "../controllers/CategoryControlle.js"
 const router = express.Router()
 
 router.get("/api/get-category", verifyToken, getCategory)
 router.post("/api/create-category", verifyToken, createCategory)
+router.put("/api/update-category/:id", verifyToken, updateCategory)
+router.delete("/api/delete-category/:id", verifyToken, deleteCategory)
 
 
 export default router
