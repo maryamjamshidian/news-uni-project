de36c898476d22582c0322b3de88a0fcc442b3fb
maryam <marryam.jam78@gmail.com>
2023-09-27 22:44:24 +0330

    add getAlllVideo route & getSingleVideo route

--- backend/routes/videoRoutes.js
+++ backend/routes/videoRoutes.js
@@ -1,10 +1,14 @@
 import express from "express"
 import {verifyToken} from "../middleware/VerifyTokne.js"
-import { createVideo } from "../controllers/VideoController.js";
+import { createVideo, deleteVideo, getAlllVideo, getSingleVideo } from "../controllers/VideoController.js";
 
 const router = express.Router()
 
 
 router.post("/api/create-video", verifyToken, createVideo)
+router.get('/api/get-video', verifyToken, getAlllVideo)
+router.get("/api/single-video", getSingleVideo)
+router.delete("/api/delete-video/:id", verifyToken, deleteVideo)
+
 
 export default router;
