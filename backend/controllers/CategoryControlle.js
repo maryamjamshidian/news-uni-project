07d66a1fc940d602a8407f8c69f0e7a57813bda2
maryam <marryam.jam78@gmail.com>
2023-09-27 07:44:05 +0330

    add CreateCategory fuC & updateCat & deleteCat

--- backend/controllers/CategoryControlle.js
+++ backend/controllers/CategoryControlle.js
@@ -10,9 +10,39 @@ export const getCategory = async(req,res)=> {
 }
 
 export const createCategory = async(req,res) => {
+          const name = req.body.name;
      try {
-          res.json("create category")
+          await Category.create({
+               name: name,
+          })
+          res.json("دسته بندی افزوده شد")
      } catch (error) {
           console.log(object);
+     }
+}
+
+export const updateCategory = async(req,res)=> {
+     const name = req.body.name;
+     try {
+          await Category.update({name: name},{
+               where: {id: req.params.id}
+          })
+          res.json("ویرایش با موفقیت انجام شد")
+     } catch (error) {
+          console.log(error);
+     }
+}
+
+
+export const deleteCategory = async(req,res)=> {
+     try {
+         await Category.destroy({
+              where: {
+                   id: req.params.id
+              }
+         })
+         res.json("حذف دسته بندی موفقیت آمیز بود")
+     } catch (error) {
+          console.log(error);
      }
 }
