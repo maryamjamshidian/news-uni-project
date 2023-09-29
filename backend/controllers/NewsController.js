b2abc2c1198a01c2f47186925e13d9213cbb5996
maryam <marryam.jam78@gmail.com>
2023-09-29 22:51:35 +0330

    add updateNews & delete &  createNews &getNewsById

--- backend/controllers/NewsController.js
+++ backend/controllers/NewsController.js
@@ -1,63 +1,148 @@
-import News from "../models/newsModel.js"
+import News from "../models/newsModel.js";
 import path from "path";
-import fs from "fs"
-export const getNews = async(req,res) => {
-     try {
-          const news = await News.findAll({})
-          res.json(news)
-     } catch (error) {
-          console.log(error)
-     }
-}
+import fs from "fs";
+export const getNews = async (req, res) => {
+  try {
+    const news = await News.findAll({});
+    res.json(news);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createNews = async (req, res) => {
+  if (req.files == null) return res.json({ error: "عکسی انتخاب نکردید" });
+  const title = req.body.title;
+  const desc = req.body.desc;
+  const catId = req.body.catId;
+  const userId = req.body.userId;
+  const file = req.files.file;
+  const fileSize = file.data.length;
+  const ext = path.extname(file.name);
+  let dateNow = Math.round(Date.now());
+  const fileName = dateNow + ext;
+  const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+  const allowedType = [".png", ".jpg", ".jpeg"];
+
+  if (!allowedType.includes(ext.toLowerCase())) {
+    return res.json({
+      msg: " .png .jpeg .jpg *  عکس نامعتبر است از فرمت مجاز استفاده کنید ",
+    });
+  }
+  if (fileSize > 5000000)
+    return res.json({ msg: "حجم عکس نباید بیشتر از 5 مگابایت باشد" });
+
+  file.mv(`./public/images/${fileName}`, async (err) => {
+    if (err) return res.json({ msg: err.message });
+    try {
+      await News.create({
+        title: title,
+        desc: desc,
+        catId: catId,
+        userId: userId,
+        image: fileName,
+        url: url,
+      });
+      res.json({ msg: "خبر با موفقیت آپلود شد." });
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+};
+
+export const getNewsById = async (req, res) => {
+  try {
+    const response = await News.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateNews = async (req, res) => {
+  const news = await News.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!news) return res.json({ msg: "دیتایی وجود ندارد" });
+
+  let fileName = "";
+  if (req.files === null) {
+    fileName = news.image;
+  } else {
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name);
+    let dateNow = Math.round(Date.now());
+    fileName = dateNow + ext;
+    const allowedType = [".png", ".jpg", ".jpeg"];
+
+    if (!allowedType.includes(ext.toLowerCase())) {
+      return res.json({
+        msg: " .png .jpeg .jpg *  عکس نامعتبر است از فرمت مجاز استفاده کنید ",
+      });
+    }
+    if (fileSize > 5000000) return res.json({ msg: "حجم عکس نباید بیشتر از 5 مگابایت باشد" });
 
 
-export const createNews = async(req,res)=>{
-     if(req.files == null) return res.json({error: "عکسی انتخاب نکردید"})
-     const title = req.body.title;
-     const desc = req.body.desc;
-     const catId = req.body.catId;
-     const userId = req.body.userId;
-     const file = req.files.file;
-     const fileSize = file.data.length;
-     const ext = path.extname(file.name);
-     let dateNow = Math.round(Date.now());
-     const fileName = dateNow + ext;
-     const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
-     const allowedType  = ['.png', ".jpg", ".jpeg"];
+      const filePath = `./public/images/${news.image}`;
+      fs.unlinkSync(filePath)
+  
+      file.mv(`./public/images/${fileName}`, (err) => {
+           if(err) return res.json({msg: err.message})
+      })
+    }
 
-     if(!allowedType.includes(ext.toLowerCase())){
-          return res.json({msg: " .png .jpeg .jpg *  عکس نامعتبر است از فرمت مجاز استفاده کنید "})
-     }
-     if(fileSize > 5000000) return res.json({msg: "حجم عکس نباید بیشتر از 5 مگابایت باشد"})
+    const title = req.body.title;
+    const desc = req.body.desc;
+    const userId = req.body.userId;
+    const catId = req.body.catId;
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
 
-     file.mv(`./public/images/${fileName}`, async(err)=> {
-          if(err) return res.json({msg: err.message})
-          try {
-               await News.create({
-                    title: title,
-                    desc: desc,
-                    catId: catId,
-                    userId: userId,
-                    image: fileName,
-                    url: url
-               })
-               res.json({msg: "خبر با موفقیت آپلود شد."})
-          } catch (error) {
-             console.log(error.message);  
-          }
+    try {
+         await News.update({
+              title: title,
+              desc: desc,
+              userId: userId,
+              catId: catId,
+              image: fileName,
+              url: url
+         }, {
+              where: {id: req.params.id}
+         })
+         res.json({msg: "خبر با موفقیت ویرایش شد."})
+    } catch (error) {
+         console.log(error);
+    }
+
+};
+
+
+export const deleteNews = async(req,res)=>{
+     const news = await News.findOne({
+       where:{
+         id: req.params.id
+       }
      })
-
-}
-
-export const getNewsById = async(req,res)=>{
+   
+     if(!news) return res.json({msg: "این خبر پیدا نشد"})
+   
      try {
-          const response = await News.findOne({
-               where: {
-                    id: req.params.id
-               }
-          })
-          res.json(response)
+         const filePath = `./public/images/${news.image}`;
+         fs.unlinkSync(filePath)
+         await News.destroy({
+           where: {
+             id: req.params.id
+           }
+         })
+         res.json({msg: "خبر با موفقیت حذف شد"})
      } catch (error) {
-          console.log(error);
+       console.log(error);
      }
-}
+   };
+   
