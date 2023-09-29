1729eedfd2d76c5953363a524f0e7c1d71bfc728
maryam <marryam.jam78@gmail.com>
2023-09-29 22:52:22 +0330

    add update & deleteNews Route

--- backend/routes/newsRoute.js
+++ backend/routes/newsRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createNews, getNews, getNewsById } from "../controllers/NewsController.js"
+import { createNews, deleteNews, getNews, getNewsById, updateNews } from "../controllers/NewsController.js"
 import {verifyToken} from "../middleware/VerifyTokne.js"
 
 const router = express.Router()
@@ -9,6 +9,8 @@ const router = express.Router()
 router.get("/api/news", verifyToken, getNews)
 router.post("/api/news", verifyToken,createNews )
 router.get("/api/news/:id", verifyToken , getNewsById)
+router.put("/api/news/:id", verifyToken, updateNews)
+router.delete("/api/news/:id", verifyToken, deleteNews)
 
 
 export default router;
