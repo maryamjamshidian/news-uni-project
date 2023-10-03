f3bb86e62a003a7f10d231131e5c124fd41f4a17
maryam <marryam.jam78@gmail.com>
2023-10-03 23:39:49 +0330

    Create Comment Route

--- backend/routes/commentRoute.js
+++ backend/routes/commentRoute.js
@@ -1,12 +1,14 @@
 import express from "express"
-import { createComment, deleteComment, getAllComment, updateComment } from "../controllers/CommentController.js";
+import { active, createComment, deleteComment, getAllComment, getComment, unActive, updateComment } from "../controllers/CommentController.js";
 import {verifyToken} from "../middleware/VerifyTokne.js"
 
 
 const router = express.Router()
-
+router.get("/api/comment/:newsId", getComment)
 router.get("/api/comment", verifyToken, getAllComment)
 router.post("/api/comment", createComment)
+router.put("/api/comment/active/:id", verifyToken, active)
+router.put("/api/comment/unactive/:id", verifyToken, unActive )
 router.put("/api/comment/:id", verifyToken, updateComment)
 router.delete("/api/comment/:id", verifyToken, deleteComment)
 
