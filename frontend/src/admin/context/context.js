233ea30d2e3a51ce31cd919967087bc974ea2aac
maryam <marryam.jam78@gmail.com>
2023-10-04 23:52:04 +0330

    createContext

---
+++ frontend/src/admin/context/context.js
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+
+
+export const AuthContext = createContext();
+
+
+export const AuthContextProvider = ({children}) => {
+
+     const login = async(inputs)=>{
+          try {
+               console.log(inputs);
+          } catch (error) {
+               console.log(error);
+          }
+     }
+
+     return (
+          <AuthContext.Provider value={{ login }}>
+               {children}
+          </AuthContext.Provider>
+     )
+}
