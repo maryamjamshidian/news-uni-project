fead97db25a17c9b65a0ea7b42f470f539cf6b5c
maryam <marryam.jam78@gmail.com>
2023-10-05 00:50:31 +0330

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import Login from "./admin/auth/Login";
-
+import {Routes, Route} from "react-router-dom";
+import Dashboard from "./admin/dashboard/Dashboard";
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-  <div>
-
-<Login/>
-
-    </div>
+    <>
+      <Routes>
+        <Route path="/administrator" element={<Login />}  />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
