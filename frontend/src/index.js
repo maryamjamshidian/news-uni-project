3059093398649c4fad9c2bf36228597b3fe4f07e
maryam <marryam.jam78@gmail.com>
2023-10-05 00:50:51 +0330

    add axios & router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,22 @@ import "bulma/css/bulma.css";
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './admin/context/context';
+import axios from 'axios';
+import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 
+axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-     <App />
-    </AuthContextProvider>
+      <BrowserRouter>
+       <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+     </BrowserRouter>
   </React.StrictMode>
 );
 
