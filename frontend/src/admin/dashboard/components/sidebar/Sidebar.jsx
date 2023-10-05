291b0cb7083aa0f9d02d7fba7e7647404ab468b0
maryam <marryam.jam78@gmail.com>
2023-10-06 00:20:40 +0330

    Create sidebar Component

---
+++ frontend/src/admin/dashboard/components/sidebar/Sidebar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./sidebar.css";
+import logo from "../../../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+const Sidebar = () => {
+  const [showNews, setShowNews] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="logo mb-5 has-text-centered">
+        <img src={logo} alt="" />
+      </div>
+      <ul>
+        <li>
+          <Link to="/dashboard">داشبورد</Link>
+        </li>
+        <li>
+          <span onClick={()=> setShowNews(!showNews)}>اخبار</span>
+
+          {showNews && (
+            <ul>
+              <li>
+                <Link to="/add-news">افزودن خبر</Link>
+              </li>
+              <li>
+                <Link to="/view-news">مشاهده خبر</Link>
+              </li>
+            </ul>
+          )}
+        </li>
+        <li>
+          <Link to="">دسته بندی</Link>
+        </li>
+        <li>
+          <Link to="">ویدیو</Link>
+        </li>
+        <li>
+          <Link to="">کاربران</Link>
+        </li>
+        <li>
+          <Link to="">نظرات</Link>
+        </li>
+        <li>
+          <Link to="">خروج</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
