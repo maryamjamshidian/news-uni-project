84bf097b42f74179e607b4d4c1dd0065130d09b8
maryam <marryam.jam78@gmail.com>
2023-10-06 00:15:15 +0330

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import Login from "./admin/auth/Login";
 import {Routes, Route} from "react-router-dom";
 import Dashboard from "./admin/dashboard/Dashboard";
+import ViewNews from "./admin/dashboard/components/news/ViewNews"
+import AddNews from "./admin/dashboard/components/news/AddNews"
 import { ToastContainer } from 'react-toastify';
+import Main from "./admin/dashboard/components/main/Main";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/administrator" element={<Login />}  />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Main />} />
+        <Route path="/add-news" element={<AddNews />} />
+        <Route path="/view-news" element={<ViewNews />} />
       </Routes>
       <ToastContainer />
     </>
