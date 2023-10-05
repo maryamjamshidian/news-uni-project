ef194b237dcd952ab12ea2456ab99a738805b222
maryam <marryam.jam78@gmail.com>
2023-10-06 00:16:48 +0330

    add useEffect&jwt_decode&getAllUsers

--- frontend/src/admin/context/context.js
+++ frontend/src/admin/context/context.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useState } from "react";
 import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify"
+import { useEffect } from "react";
+import jwt_decode from "jwt-decode";
 
 export const AuthContext = createContext();
 
@@ -14,9 +16,49 @@ export const AuthContextProvider = ({children}) => {
      const [userId, setUserId] = useState("")
      const [token, setToken] = useState("")
      const [admin, setAdmin] = useState(null)
+     const [expire, setExpire] = useState("")
      const navigate = useNavigate();
 
-     console.log(error);
+     useEffect(()=> {
+          refreshToken()
+     }, [])
+     const refreshToken = async () => {
+          try {
+               const response = await axios.get("http://localhost:5000/token")
+               setToken(response.data.accessToken)
+               const decoded = jwt_decode(response.data.accessToken)
+               setName(decoded.name)
+               setUserId(decoded.userId)
+               setAdmin(decoded.isAdmin)
+               setExpire(decoded.exp)
+          } catch (error) {
+               console.log(error);
+          }
+     }
+
+     const axiosJWT  = axios.create();
+
+     axiosJWT.interceptors.request.use(
+          async (config) => {
+               const currentDate = new Date();
+               if(expire * 1000 < currentDate.getTime()){
+                    const response = await axios.get("http://localhost:5000/token")
+                    config.headers.Authorization = `Bearer ${response.data.accessToken}`
+                    setToken(response.data.accessToken)
+                    const decoded = jwt_decode(response.data.accessToken)
+                    setName(decoded.name)
+                    setUserId(decoded.userId)
+                    setAdmin(decoded.isAdmin)
+                    setExpire(decoded.exp)
+               }
+               return config;
+          }, (error)=> {
+               return Promise.reject(error)
+          }
+     )
+
+
+
 
      const login = async(inputs)=>{
           try {
@@ -41,8 +83,21 @@ export const AuthContextProvider = ({children}) => {
           }
      }
 
+     const getAllUsers = async()=>{
+          try {
+               const res = await axiosJWT.get("http://localhost:5000/api/users", {
+                    headers:{
+                         authorization : `Bearer ${token}`
+                    }
+               })
+               console.log(res);
+          } catch (error) {
+             console.log(error);  
+          }
+     }
+
      return (
-          <AuthContext.Provider value={{ login, error }}>
+          <AuthContext.Provider value={{ login, error,getAllUsers }}>
                {children}
           </AuthContext.Provider>
      )
