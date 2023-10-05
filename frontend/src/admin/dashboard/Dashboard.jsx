17a1d97623b76331236e098322a1187f9d94ae10
maryam <marryam.jam78@gmail.com>
2023-10-06 00:17:12 +0330

    طراحی داشبورد

--- frontend/src/admin/dashboard/Dashboard.jsx
+++ frontend/src/admin/dashboard/Dashboard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
-const Dashboard = () => {
+import { useContext } from 'react'
+import { AuthContext } from '../context/context'
+import Sidebar from './components/sidebar/Sidebar'
+import Information from './components/information/Information'
+import "./index.css"
+import Main from './components/main/Main'
+const Dashboard = ({children}) => {
   return (
-    <div>Dashboard</div>
+    <div className="dashboard-wrapper">
+      <Sidebar />
+      <div className="main-info">
+        <Information />
+        <div className="main">
+           {children}
+        </div>
+      </div>
+    </div>
   )
 }
 
