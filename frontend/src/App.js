70457638460eed5c9c4ad2e5dad654927ed1dcc2
maryam <marryam.jam78@gmail.com>
2023-10-07 17:57:42 +0330

    EditCategory & AddCategory&ViewCategory&EditNews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import ViewNews from "./admin/dashboard/components/news/ViewNews"
 import AddNews from "./admin/dashboard/components/news/AddNews"
 import { ToastContainer } from 'react-toastify';
 import Main from "./admin/dashboard/components/main/Main";
+import EditNews from "./admin/dashboard/components/news/EditNews";
+import ViewCategory from "./admin/dashboard/components/category/ViewCategory";
+import AddCategory from "./admin/dashboard/components/category/AddCategory";
+import EditCategory from "./admin/dashboard/components/category/EditCategory";
 
 function App() {
   return (
@@ -14,6 +18,11 @@ function App() {
         <Route path="/dashboard" element={<Main />} />
         <Route path="/add-news" element={<AddNews />} />
         <Route path="/view-news" element={<ViewNews />} />
+        <Route path="/edit-news/:id" element={<EditNews />} />
+
+        <Route path="/view-category" element={<ViewCategory />} />
+        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="/update-category/:id" element={<EditCategory />} />
       </Routes>
       <ToastContainer />
     </>
