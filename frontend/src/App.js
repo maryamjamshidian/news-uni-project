6b19e6c91054d0aa4699d756cf872caee4b8e2ff
maryam <marryam.jam78@gmail.com>
2023-10-10 00:31:35 +0330

    add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,12 @@ import EditNews from "./admin/dashboard/components/news/EditNews";
 import ViewCategory from "./admin/dashboard/components/category/ViewCategory";
 import AddCategory from "./admin/dashboard/components/category/AddCategory";
 import EditCategory from "./admin/dashboard/components/category/EditCategory";
+import ViewVideo from "./admin/dashboard/components/video/ViewVideo";
+import AddVideo from "./admin/dashboard/components/video/AddVideo";
+import ViewUsers from "./admin/dashboard/components/users/ViewUsers";
+import AddUsers from "./admin/dashboard/components/users/AddUsers";
+import EditUsers from "./admin/dashboard/components/users/EditUsers";
+import ProfileUpdate from "./admin/dashboard/components/users/ProfileUpdate";
 
 function App() {
   return (
@@ -23,6 +29,15 @@ function App() {
         <Route path="/view-category" element={<ViewCategory />} />
         <Route path="/add-category" element={<AddCategory />} />
         <Route path="/update-category/:id" element={<EditCategory />} />
+
+        <Route path="/view-video" element={<ViewVideo />} />
+        <Route path="/add-video" element={<AddVideo />} />
+
+
+        <Route path="/view-users" element={<ViewUsers />} />
+        <Route path="/add-user" element={<AddUsers />} />
+        <Route path="/edit-user/:id" element={<EditUsers />} />
+        <Route path="/update-profile/:id" element={<ProfileUpdate />} />
       </Routes>
       <ToastContainer />
     </>
